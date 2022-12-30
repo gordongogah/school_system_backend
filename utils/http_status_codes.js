81c01dc464923b1945f51d689390dae9e3dbd787
Meltus645 <meltusjuma5@gmail.com>
2022-12-30 11:56:07 +0300

    created the http status codes for the project

---
+++ utils/http_status_codes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export const HTTP_200_OK =200;
+export const HTTP_201_CREATED =201;
+export const HTTP_204_NO_CONTENT =204;
+export const HTTP_400_BAD_REQUEST =400;
+export const HTTP_404_NOT_FOUND =404;
+export const HTTP_409_CONFLICT =409;
+export const HTTP_500_SERVER_ERROR =500;
