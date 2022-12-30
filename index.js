6cd85688abb21fc1cee52f6826c6cfedb35976cb
Meltus645 <meltusjuma5@gmail.com>
2022-12-30 12:02:21 +0300

    link backend express server to mysql dbs server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
-import dotenv from "dotenv";
-import db from "./config/Database.js";
-import { flatMap } from "lodash";
-dotenv.config();
+import dotenv from "dotenv"; 
+import db from './config/instance.db.js';
 
+// import routes here
+
+dotenv.config(); 
 const app = express();
+const PORT =process.env.APP_PORT || 5000;
 
+app.use(express.json({limit: '30mb', extended: true}));
+app.use(express.urlencoded({limit: '30mb', extended: true}));
+app.use(cors()); 
+ 
 app.use(session({
     secret:process.env.SESS_SECRET,
     resave:false,
@@ -15,13 +21,12 @@ app.use(session({
     cookie:{
         secure:'auto'
     }
-}))
+}))  
 
-app.use(cors({
-    credentials:true,
-    origin:"http://localhost:3000"
-}));
-app.use(express.json());
-app.listen(process.env.APP_PORT,()=>{
-    console.log('Server up and running....');
-});
+
+// install routes here
+
+// connect to db instance
+db.sequelize.sync()
+.then(() =>app.listen(PORT,()=> console.log(`[+] Server running on port ${PORT}`)))
+.catch(({message}) =>console.log(`[-] ${message}`));
